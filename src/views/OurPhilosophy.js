e2817c1721b7cb20c0f8e1fe5885a4cf7d25e725
Serhii Burdeinyi <tibtus@gmail.com>
2024-02-26 10:30:25 +0200

    #OurPhilosophy_Text_Text_v2

--- src/views/OurPhilosophy.js
+++ src/views/OurPhilosophy.js
@@ -11,8 +11,8 @@ const OurPhilosophy = () => {
             </div>
 
             <div className="OurPhilosophy_Text">
-                Our company’s philosophy is based on three pillars: responsibility, <br/>
-                efficiency, and professionalism, while our people are the backbone of the <br/>
+                Our company’s philosophy is based on three pillars: responsibility,
+                efficiency, and professionalism, while our people are the backbone of the
                 business. Driven forward by our motivated and caring team, we strive to achieve the best results for
                 our customers by using cutting-edge technologies and continuously improving our processes.
             </div>
