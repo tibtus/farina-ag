a10e7a93bf19ebe8293727c4bb2a005fb8b032fc
Serhii Burdeinyi <tibtus@gmail.com>
2024-03-08 09:26:27 +0200

    #Hero_currentLanguage_de

--- src/views/Hero.js
+++ src/views/Hero.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import frame from './../assets/png/Frame_4_desc.jpg';
+import {useLanguage} from "../Language";
 
 const Hero = () => {
+    const {currentLanguage} = useLanguage();
+
     return (
         <section className="heroBlock">
             <div className="heroBlock_logo"  style={{ backgroundImage: `url(${frame})` }}>
                 {/*<img src={frame} alt="frame" />*/}
 
-                <div className="heroBlock_text">
-                    Your reliable partner in construction <br />
-                    and development
+                <div className="heroBlock_text" style={{whiteSpace: 'pre-line'}}>
+                    {currentLanguage === 'de'
+                        ? 'Ihr zuverlässiger Partner für Konstruktion\n' +
+                        'und Entwicklun'
+                        : 'Your reliable partner in construction\n' +
+                        'and development'
+                    }
                 </div>
             </div>
         </section>
